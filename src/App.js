7617c0d0bfd672a6580727414f3c8689057cdb01
Andrei <andrei.vida@protonmail.com>
2023-01-07 22:54:17 +0200

    Modif2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "./Header"
+import Memez1 from './Memez1'
 import './index.css'
 /**
  * Challenge: Build the Header component
@@ -8,6 +9,7 @@ export default function App() {
     return (
         <div>
             <Header />
+            <Memez1 />
         </div>
     )
 }
